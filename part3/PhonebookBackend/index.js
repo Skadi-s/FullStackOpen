60abace192e64419ef7195874af9e12bb8d6a257
jiangbl <jiangbl@qq.com>
2025-06-30 16:48:13 +0800

    learn part3 express

---
+++ part3/PhonebookBackend/index.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const app = express()
+
+let phonebook = [
+    {
+      "id": 1,
+      "name": "Arto Hellas",
+      "number": "040-123456"
+    },
+    {
+      "id": 2,
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523"
+    },
+    {
+      "id": 3,
+      "name": "Dan Abramov",
+      "number": "12-43-234345"
+    },
+    {
+      "id": 4,
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Phonebook</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(phonebook)
+})
+
+app.get('/info', (request, response) => { 
+  const date = new Date()
+  const count = phonebook.length
+  response.send(`<p>Phonebook has info for ${count} people</p><p>${date}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = phonebook.find(p => p.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).send({ error: 'Person not found' })
+  }
+})
+
+app.post('/api/persons', express.json(), (request, response) => {
+  const newPerson = request.body
+  if (!newPerson.name || !newPerson.number) {
+    return response.status(400).json({ error: 'Name or number is missing' })
+  }
+  
+  const existingPerson = phonebook.find(p => p.name === newPerson.name)
+  if (existingPerson) {
+    return response.status(400).json({ error: 'Name must be unique' })
+  }
+
+  const id = Math.max(...phonebook.map(p => p.id)) + 1
+  const personToAdd = { ...newPerson, id }
+  phonebook.push(personToAdd)
+  response.status(201).json(personToAdd)
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})

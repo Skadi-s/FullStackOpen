941827123e6e923558b2753f7d5dcf279f3a896c
skadi-pc <1632504075@qq.com>
2025-07-01 22:43:27 +0800

    use mongo db

---
+++ part3/PhonebookBackend/mongoose.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://jiangbl:${password}@cluster0.aud6j.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})

b0b47370bf530630428cc85a979e25aabc62dfe0
skadi-pc <1632504075@qq.com>
2025-07-01 18:24:06 +0800

    deploy on render and change baseurl

--- part2/phonebook/src/services/phones.js
+++ part2/phonebook/src/services/phones.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/persons';
+const baseUrl = 'https://phonebook-backend-v74n.onrender.com/api/persons';
 
 const getAll = () => {
   return axios.get(baseUrl);

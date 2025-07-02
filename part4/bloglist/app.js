f2cf7f30d22c76d405d2229e83621353e224704d
skadi-pc <1632504075@qq.com>
2025-07-03 02:16:48 +0800

    feat(api): add user and userRouter

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,11 +1,12 @@
-const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 
 mongoose.set('strictQuery', false)
@@ -26,6 +27,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)

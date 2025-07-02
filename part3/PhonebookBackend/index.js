9cbde6e0c64728b94412362e9e5b861e10d126bb
jiangbl <jiangbl@qq.com>
2025-07-02 14:15:33 +0800

    feat(middleware):add error handle middleware

--- part3/PhonebookBackend/index.js
+++ part3/PhonebookBackend/index.js
@@ -97,6 +97,19 @@ app.delete('/api/persons/:id', (request, response) => {
   })
 })
 
+// 错误处理中间件
+app.use((error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  response.status(500).json({ error: 'Internal server error' })
+})
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)

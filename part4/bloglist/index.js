adf15eacaf991b2bb658b059a4c1a67a667a0773
skadi-pc <1632504075@qq.com>
2025-07-02 17:46:52 +0800

    fix bugs

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,7 +1,10 @@
-const app = require('./app') // the actual Express application
+// const app = require('./app') // the actual Express application
 const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+
+const express = require('express')
+const app = express()
 
 const server = http.createServer(app)
 

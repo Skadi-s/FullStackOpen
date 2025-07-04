6366fb3c48907a7bc85b65923e647ecd8a1da266
jiangbl <jiangbl@qq.com>
2025-07-04 16:15:53 +0800

    blog display sort by likes

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -96,7 +96,7 @@ const App = () => {
             <BlogForm createBlog={createBlog} />
           </Togglable>
         </div>
-        {blogs.map(blog =>
+        {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
           <Blog key={blog.id} blog={blog} likeBlog={likeBlog} />
         )}
       </div>

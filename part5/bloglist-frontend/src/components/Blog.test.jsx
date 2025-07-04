a8e1a46acad8bf7147ba0c7c740ab4ed93ac9a2f
jiangbl <jiangbl@qq.com>
2025-07-04 16:59:29 +0800

    add jtest

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -10,11 +10,7 @@ test("renders blog title and author", () => {
     user: { name: "Test User" }
   }
 
-  // 提供模拟函数作为 props
-  const mockLikeBlog = jest.fn()
-  const mockRemoveBlog = jest.fn()
+  render(<Blog blog={blog} />)
 
-  render(<Blog blog={blog} likeBlog={mockLikeBlog} removeBlog={mockRemoveBlog} />)
-  
   expect(screen.getByText("Test Blog by Test Author")).toBeInTheDocument()
 })

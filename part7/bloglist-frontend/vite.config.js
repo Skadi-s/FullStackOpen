38b9322254598502bf4f7f46283235827a5627ea
jiangbl <jiangbl@qq.com>
2025-07-11 10:37:54 +0800

    init

--- part7/bloglist-frontend/vite.config.js
+++ part7/bloglist-frontend/vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './testSetup.js',
-  },
   plugins: [react()],
 })
